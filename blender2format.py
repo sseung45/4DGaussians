92b3f3c62e2d414e8e100eb9d5b47062a29678d5
sseung45 <sseung45@postech.ac.kr>
2024-03-21 01:51:44 +0900

    blender를 format에 맞게게

---
+++ blender2format.py
@@ -0,0 +1,39 @@
+import os
+import json
+from PIL import Image
+import shutil
+
+data_path = "./data/dnerf/trex/"
+
+def formatting(split):
+    with open(data_path+"transforms_"+split+".json") as f:
+        old_transforms = json.load(f)
+    new_transforms = dict()
+    new_transforms["camera_angle_x"] = old_transforms["camera_angle_x"]
+    new_transforms["camera_angle_y"] = old_transforms["camera_angle_x"]
+    new_transforms["frames"] = []
+
+    for idx, frame in enumerate(old_transforms['frames']):
+        new_frame = {}
+        old_imgname = data_path+frame['file_path']+".png"
+        new_imgname = "./"+split+"/frame"+str(idx).zfill(6)+".png"
+        image = Image.open(old_imgname)
+        shutil.copy(old_imgname, data_path+new_imgname)
+        new_frame["file_path"] = new_imgname
+        new_frame["time"] = frame["time"]
+        new_frame["height"] = image.size[1]
+        new_frame["width"] = image.size[0]
+        new_frame["transform_matrix"] = frame["transform_matrix"]
+
+        os.remove(old_imgname)
+        new_transforms["frames"].append(new_frame)
+    
+    with open(data_path+"/"+split+"_transforms"+".json", "w") as outfile:
+	    json.dump(new_transforms, outfile, indent=4)
+         
+    os.remove(data_path+"transforms_"+split+".json")
+
+formatting("train")
+formatting("test")
+formatting("val")
+
