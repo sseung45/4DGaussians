8b295e475cd54c687bcf7ee30a47a12a43683f5c
sseung45 <sseung45@postech.ac.kr>
2024-03-22 03:20:58 +0900

    직전 커밋 수정

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -298,8 +298,8 @@ def readCamerasFromTransforms(path, transformsfile, white_background, extension=
                 h = image.size[1]
                 image = PILtoTorch(image,None)
                 image = image.to(torch.float32)[:3,:,:]
-                FovY = focal2fov(fov2focal(contents["camera_angle_y"],2*h), 2*h)
-                FovX = focal2fov(fov2focal(contents["camera_angle_x"],2*w), 2*w)
+                FovY = focal2fov(fov2focal(contents["camera_angle_y"],2*h), h)
+                FovX = focal2fov(fov2focal(contents["camera_angle_x"],2*w), w)
                 cam_infos.append(CameraInfo(uid=idx, R=R, T=T, FovY=FovY, FovX=FovX, image=image,
                             image_path=image_path, image_name=image_name, width=w, height=h,
                             time = time, mask=None))
